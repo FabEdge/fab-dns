39b391cdc15e658a52679540e0bdfb6631830c2c
Llishiwei <lishiwei@beyondcent.com>
2021-12-29 15:39:53 +0800

    fabdns: add request parsing and corresponding tests

--- pkg/fabdns/fabdns.go
+++ pkg/fabdns/fabdns.go
@@ -1,14 +1,33 @@
-package dns
+package fabdns
 
 import (
 	"context"
+	"errors"
+	"fmt"
 
 	"github.com/coredns/coredns/plugin"
 	"github.com/coredns/coredns/plugin/pkg/fall"
+	clog "github.com/coredns/coredns/plugin/pkg/log"
+	"github.com/coredns/coredns/request"
 	"github.com/miekg/dns"
 )
 
-const defaultTTL = uint32(5)
+const (
+	// Svc is the DNS schema for global services
+	Svc = "svc"
+	// Pod is the DNS schema for kubernetes pods
+	Pod        = "pod"
+	defaultTTL = uint32(5)
+)
+
+var (
+	errNoItems        = errors.New("no items found")
+	errInvalidRequest = errors.New("invalid query name")
+)
+
+// Define log to be a logger with the plugin name in it. This way we can just use log.Info and
+// friends to log.
+var log = clog.NewWithPlugin(PluginName)
 
 // FabDNS implements a plugin supporting multi-cluster FabDNS spec.
 type FabDNS struct {
@@ -36,8 +55,47 @@ func New(zones []string, cluster, clusterZone, clusterRegion string) (*FabDNS, e
 
 // ServeDNS implements the plugin.Handler interface.
 func (f FabDNS) ServeDNS(ctx context.Context, w dns.ResponseWriter, r *dns.Msg) (int, error) {
+	state := request.Request{W: w, Req: r}
 
-	return dns.RcodeSuccess, nil
+	qname := state.QName()
+
+	log.Debugf("Request query name is %s", qname)
+
+	zone := plugin.Zones(f.Zones).Matches(qname)
+	if len(zone) == 0 {
+		log.Debugf("Request query name does not match zones %v", f.Zones)
+		return f.nextOrFailure(&state, ctx, w, r, dns.RcodeNotZone, errors.New("name not contained in zone"))
+	}
+	zone = qname[len(qname)-len(zone):] // maintain case of original query
+	state.Zone = zone
+
+	if state.QType() != dns.TypeA && state.QType() != dns.TypeAAAA {
+		log.Debugf("query type %d is not implemented", state.QType())
+		return f.nextOrFailure(&state, ctx, w, r, dns.RcodeNotImplemented, fmt.Errorf("query type %d is not implemented", state.QType()))
+	}
+
+	var (
+		parsedReq recordRequest
+		records   []dns.RR
+		err       error
+	)
+
+	parsedReq, err = parseRequest(qname, zone)
+	if err != nil {
+		log.Debugf("parse request err: %v", err)
+		return f.nextOrFailure(&state, ctx, w, r, dns.RcodeNameError, err)
+	}
+
+	records, err = f.getRecords(&state, parsedReq)
+	if f.IsNameError(err) {
+		log.Debugf("get records err: %v", err)
+		return f.nextOrFailure(&state, ctx, w, r, dns.RcodeNameError, err)
+	}
+	if err != nil {
+		return dns.RcodeServerFailure, plugin.Error(f.Name(), err)
+	}
+
+	return f.writeMsg(&state, records, dns.RcodeSuccess, nil)
 }
 
 // Name implements the Handler interface.
@@ -45,16 +103,40 @@ func (f FabDNS) Name() string {
 	return PluginName
 }
 
-type ResponsePrinter struct {
-	dns.ResponseWriter
+// IsNameError returns true if err indicated a record not found condition
+func (f FabDNS) IsNameError(err error) bool {
+	return err == errNoItems || err == errInvalidRequest
 }
 
-// NewResponsePrinter returns ResponseWriter.
-func NewResponsePrinter(w dns.ResponseWriter) *ResponsePrinter {
-	return &ResponsePrinter{ResponseWriter: w}
+func (f FabDNS) getRecords(state *request.Request, parsedReq recordRequest) ([]dns.RR, error) {
+	switch state.QType() {
+	case dns.TypeA:
+	case dns.TypeAAAA:
+	}
+	return nil, nil
 }
 
-// WriteMsg calls the underlying ResponseWriter's WriteMsg method and prints "example" to standard output.
-func (r *ResponsePrinter) WriteMsg(res *dns.Msg) error {
-	return r.ResponseWriter.WriteMsg(res)
+func (f FabDNS) writeMsg(state *request.Request, records []dns.RR, rcode int, err error) (int, error) {
+	message := new(dns.Msg)
+	message.Authoritative = true
+
+	switch rcode {
+	case dns.RcodeSuccess:
+		message.SetReply(state.Req)
+		message.Answer = append(message.Answer, records...)
+	default:
+		message.SetRcode(state.Req, rcode)
+		err = plugin.Error(f.Name(), err)
+	}
+
+	state.W.WriteMsg(message)
+	return rcode, err
+}
+
+func (f FabDNS) nextOrFailure(state *request.Request, ctx context.Context, w dns.ResponseWriter, r *dns.Msg, rcode int, err error) (int, error) {
+	if f.Fall.Through(state.Name()) {
+		return plugin.NextOrFailure(f.Name(), f.Next, ctx, w, r)
+	}
+
+	return f.writeMsg(state, nil, rcode, err)
 }
--- pkg/fabdns/fabdns_test.go
+++ pkg/fabdns/fabdns_test.go
@@ -1,22 +1,39 @@
-package dns
+package fabdns
 
 import (
 	"context"
+	"errors"
 	"fmt"
 
 	"github.com/coredns/coredns/plugin/pkg/dnstest"
+	"github.com/coredns/coredns/plugin/pkg/fall"
 	"github.com/coredns/coredns/plugin/test"
 	"github.com/miekg/dns"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 )
 
+const (
+	testZone            = "testzone."
+	testFallthroughZone = "fall."
+	testUnknownZone     = "unknown."
+)
+
 var _ = Describe("Fabdns", func() {
+	Context("Implements of QType", testRequestImplements)
+	Context("Fallthrough configured", testFallthroughConfigured)
+	Context("ClusterIP services", testClusterIPServices)
+	Context("Headless services", testHeadlessServices)
+})
+
+func testRequestImplements() {
 	var (
-		recorder *dnstest.Recorder
+		qname        = fmt.Sprintf("%s.%s.svc.%s", "testservice", "testns", testZone)
+		testRecorder *dnstest.Recorder
 	)
+
 	fabdns := &FabDNS{
-		Zones:         []string{"svc.global."},
+		Zones:         []string{testZone},
 		Cluster:       "fabedge",
 		ClusterZone:   "beijing",
 		ClusterRegion: "north",
@@ -24,17 +41,195 @@ var _ = Describe("Fabdns", func() {
 	}
 
 	BeforeEach(func() {
-		recorder = dnstest.NewRecorder(&test.ResponseWriter{})
+		testRecorder = dnstest.NewRecorder(&test.ResponseWriter{})
+	})
+
+	When("Query type is A", func() {
+		It("should succeed", func() {
+			testCase := test.Case{
+				Qname: qname,
+				Qtype: dns.TypeA,
+				Rcode: dns.RcodeSuccess,
+			}
+			executeTestCase(fabdns, testRecorder, testCase)
+		})
+	})
+
+	When("Query type is AAAA", func() {
+		It("should succeed", func() {
+			testCase := test.Case{
+				Qname: qname,
+				Qtype: dns.TypeAAAA,
+				Rcode: dns.RcodeSuccess,
+			}
+			executeTestCase(fabdns, testRecorder, testCase)
+		})
+	})
+
+	When("Query type is SRV", func() {
+		It("should failed", func() {
+			testCase := test.Case{
+				Qname: qname,
+				Qtype: dns.TypeSRV,
+				Rcode: dns.RcodeNotImplemented,
+			}
+			executeTestCase(fabdns, testRecorder, testCase)
+		})
+	})
+
+	When("Query type is NS", func() {
+		It("should failed", func() {
+			testCase := test.Case{
+				Qname: qname,
+				Qtype: dns.TypeNS,
+				Rcode: dns.RcodeNotImplemented,
+			}
+			executeTestCase(fabdns, testRecorder, testCase)
+		})
+	})
+
+	When("Query type is PTR", func() {
+		It("should failed", func() {
+			testCase := test.Case{
+				Qname: qname,
+				Qtype: dns.TypePTR,
+				Rcode: dns.RcodeNotImplemented,
+			}
+			executeTestCase(fabdns, testRecorder, testCase)
+		})
+	})
+}
+
+func testFallthroughConfigured() {
+	var (
+		testRecorder *dnstest.Recorder
+		fabdns       *FabDNS
+	)
+
+	BeforeEach(func() {
+		fabdns = &FabDNS{
+			Next:          test.NextHandler(dns.RcodeBadCookie, errors.New("fake plugin error")),
+			Zones:         []string{testZone},
+			Fall:          fall.F{Zones: []string{testFallthroughZone}},
+			Cluster:       "fabedge",
+			ClusterZone:   "beijing",
+			ClusterRegion: "north",
+			TTL:           5,
+		}
+		testRecorder = dnstest.NewRecorder(&test.ResponseWriter{})
+	})
+
+	When("type A DNS query for matching fallthrough zone", func() {
+		It("should invoke next plugin", func() {
+			qname := fmt.Sprintf("%s.%s.svc.%s", "testservice", "testns", testFallthroughZone)
+			testCase := test.Case{
+				Qname: qname,
+				Qtype: dns.TypeA,
+				Rcode: dns.RcodeBadCookie,
+			}
+			_, err := executeTestCase(fabdns, testRecorder, testCase)
+			Expect(err.Error()).Should(ContainSubstring("fake plugin"))
+		})
+
+	})
+
+	When("type A DNS query for non-matching fallthrough zone", func() {
+		It("should not invoke next plugin", func() {
+			qname := fmt.Sprintf("%s.%s.svc.%s", "testservice", "testns", testUnknownZone)
+			testCase := test.Case{
+				Qname: qname,
+				Qtype: dns.TypeA,
+				Rcode: dns.RcodeNotZone,
+			}
+			_, err := executeTestCase(fabdns, testRecorder, testCase)
+			Expect(err.Error()).To(ContainSubstring("not contained in zone"))
+		})
+	})
+
+}
+
+func testClusterIPServices() {
+	var testRecorder *dnstest.Recorder
+
+	fabdns := &FabDNS{
+		Zones:         []string{testZone},
+		Cluster:       "fabedge",
+		ClusterZone:   "beijing",
+		ClusterRegion: "north",
+		TTL:           5,
+	}
+
+	BeforeEach(func() {
+		testRecorder = dnstest.NewRecorder(&test.ResponseWriter{})
 	})
 
 	It("should succeed with A record response", func() {
-		qname := fmt.Sprintf("%s.%s.svc.global", "testservice", "testns")
+		qname := fmt.Sprintf("%s.%s.svc.%s", "testservice", "testns", testZone)
 		testCase := test.Case{
 			Qname: qname,
 			Qtype: dns.TypeA,
+			Rcode: dns.RcodeSuccess,
 		}
-		rcode, err := fabdns.ServeDNS(context.TODO(), recorder, testCase.Msg())
+		executeTestCase(fabdns, testRecorder, testCase)
+	})
+
+	It("should succeed with AAAA record response", func() {
+		qname := fmt.Sprintf("%s.%s.svc.%s", "testservice", "testns", testZone)
+		testCase := test.Case{
+			Qname: qname,
+			Qtype: dns.TypeAAAA,
+			Rcode: dns.RcodeSuccess,
+		}
+		executeTestCase(fabdns, testRecorder, testCase)
+	})
+
+}
+
+func testHeadlessServices() {
+	var testRecorder *dnstest.Recorder
+
+	fabdns := &FabDNS{
+		Zones:         []string{testZone},
+		Cluster:       "fabedge",
+		ClusterZone:   "beijing",
+		ClusterRegion: "north",
+		TTL:           5,
+	}
+
+	BeforeEach(func() {
+		testRecorder = dnstest.NewRecorder(&test.ResponseWriter{})
+	})
+
+	It("should succeed with A record response", func() {
+		qname := fmt.Sprintf("%s.%s.%s.%s.svc.%s", "hostname", "mycluster", "testservice", "testns", testZone)
+		testCase := test.Case{
+			Qname: qname,
+			Qtype: dns.TypeA,
+			Rcode: dns.RcodeSuccess,
+		}
+		executeTestCase(fabdns, testRecorder, testCase)
+	})
+
+	It("should succeed with AAAA record response", func() {
+		qname := fmt.Sprintf("%s.%s.%s.%s.svc.%s", "hostname", "mycluster", "testservice", "testns", testZone)
+		testCase := test.Case{
+			Qname: qname,
+			Qtype: dns.TypeAAAA,
+			Rcode: dns.RcodeSuccess,
+		}
+		executeTestCase(fabdns, testRecorder, testCase)
+	})
+}
+
+func executeTestCase(fabdns *FabDNS, recorder *dnstest.Recorder, testcase test.Case) (rcode int, err error) {
+	rcode, err = fabdns.ServeDNS(context.TODO(), recorder, testcase.Msg())
+	Expect(rcode).To(Equal(testcase.Rcode))
+
+	if testcase.Rcode == dns.RcodeSuccess {
 		Expect(err).To(Succeed())
 		Expect(rcode).To(Equal(dns.RcodeSuccess))
-	})
-})
+	} else {
+		Expect(err).To(HaveOccurred())
+	}
+	return rcode, err
+}
