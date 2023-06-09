42879807582c8991984b3bafee4effcaa65d3c8f
yanjianbo1983 <yanjianbo@beyondcent.com>
2023-06-09 10:41:38 +0800

    upgrade endpointslice import from v1beta1 to v1
    Signed-off-by: yanjianbo1983 <yanjianbo@beyondcent.com>

--- pkg/service-hub/exporter/service_exporter_test.go
+++ pkg/service-hub/exporter/service_exporter_test.go
@@ -8,7 +8,7 @@ import (
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 	corev1 "k8s.io/api/core/v1"
-	discoveryv1 "k8s.io/api/discovery/v1beta1"
+	discoveryv1 "k8s.io/api/discovery/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/manager"
